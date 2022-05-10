2f7d99fd65db06777bea2500386c7422fb96d488
Krzysztof Potocki <kpotocki89@gmail.com>
2022-05-10 12:24:15 +0200

    Add Button and Display

---
+++ src/components/Display/Display.js
@@ -0,0 +1,12 @@
+import styles from './Display.module.scss';
+
+const Display = props => {
+
+  return (
+    <div className={styles.display}>
+
+    </div>
+  );
+};
+
+export default Display;
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+import Button from '../Button/Button';
 import styles from './Timer.module.scss';
+import Display from '../Display/Display';
 
 const Timer = props => {
+  const [time, setTime] = useState([0]); 
+
   return (
     <div className={styles.timer}>
-
+      <Display />
+      <Button>Start</Button>
+      <Button>Stop</Button>
+      <Button>Reset</Button>
     </div>
   );
 };
