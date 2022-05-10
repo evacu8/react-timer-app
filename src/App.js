51959b00c9c23e96cabcf0c35ab514d5b5e87cca
Krzysztof Potocki <kpotocki89@gmail.com>
2022-05-10 10:50:01 +0200

    Add Container and Timer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './styles/normalize.scss';
 import './styles/global.scss';
-
-
-import './styles/normalize.scss';
-import './styles/global.scss';
+import Container from './components/Container/Container';
+import Timer from './components/Timer/Timer';
 
 const App = () => {
   return (
     <div className="App">
-      react-timer-app
+      <Container>
+        <Timer />
+      </Container>
     </div>
   );
 }
---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,11 @@
+import styles from './Timer.module.scss';
+
+const Timer = props => {
+  return (
+    <div className={styles.timer}>
+
+    </div>
+  );
+};
+
+export default Timer;
