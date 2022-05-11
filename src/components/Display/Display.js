8db0b8fdcdcee40466d3f4daf4b8ee9fdd940301
Krzysztof Potocki <kpotocki89@gmail.com>
2022-05-11 10:44:05 +0200

    Add buttons functionality, Add counting up in millseconds

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -4,7 +4,7 @@ const Display = props => {
 
   return (
     <div className={styles.display}>
-
+      <div>{props.time}</div>
     </div>
   );
 };
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,17 +1,45 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 import Button from '../Button/Button';
 import styles from './Timer.module.scss';
 import Display from '../Display/Display';
 
-const Timer = props => {
-  const [time, setTime] = useState([0]); 
+const Timer = () => {
+
+  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState(null);
+
+  useEffect(() => {
+    return () => {
+       if(timer) clearInterval(timer);
+    };
+  }, [timer]);
+
+  const start = () => {
+    if(!timer){
+      setTimer(setInterval(() => {
+        setTime(prevValue => prevValue + 1);
+      }, 1))
+    }
+  };
+
+  const stop = () => {
+    clearInterval(timer);
+    setTimer(null);
+  }
+
+  const reset = () => {
+    stop();
+    setTime(0);
+    setTimer(null);
+  }
 
   return (
     <div className={styles.timer}>
-      <Display />
-      <Button>Start</Button>
-      <Button>Stop</Button>
-      <Button>Reset</Button>
+      <Display time = {time}/>
+      <Button onClick={start}>Start</Button>
+      <Button onClick = {stop}>Stop</Button>
+      <Button onClick = {reset}>Reset</Button> 
     </div>
   );
 };
